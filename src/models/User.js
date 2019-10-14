1c977d571de61f273359881fd7f03ecf9749e1a3
Alex Philayvanh <aphilay@gmail.com>
2019-10-14 01:16:19 -0700

    modified genSalt function

--- src/models/User.js
+++ src/models/User.js
@@ -31,14 +31,23 @@ userSchema.pre("save", function(next) {
     return next();
   }
 
+  // generate salt
   // 10: how complex we want the salt to be
-  bcrypt.genSalt(user.password, salt, (err, hash) => {
+  // salt: our plain text string
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err);
     }
-    // overwrite plain-text pw with resulted hash & salted pw
-    user.password = hash;
-    next();
+
+    // hash pw with salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      // overwrite plain-text pw with resulted hash & salted pw
+      user.password = hash;
+      next();
+    });
   });
 });
 
